e53b4c3e5b1bc0c08de7e6bd47152d6d65ac1267
kukuru5a <romanzag@icloud.com>
2023-06-06 23:20:35 +0600

    created a helpDesk in App file. Run the application.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//import hexlet.code.Differ;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import javax.annotation.processing.Generated;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.0.1",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Parameters(index = "0", description = "The name of file to be compared.")
+    private String filePath1;
+    @Parameters(index = "1", description = "The name of file to be compared.")
+    private String filePath2;
+
+//    @Option(names = {"-h",  "--help"}, description = "Show this help message and exit.",
+//            names = {"-V", "--version"}, description = "Print version information and exit.")
+//    private String help = "Show this help message and exit.";
+//    private String version;
+//    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+//    private String version;
+
+    @Override
+    public void run() {
+        Differ.generate(filePath1, filePath2);
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+
+import java.util.Arrays;
+
+public class Differ   {
+    public static String generate (String n, String m) {
+//
+        return "HW";
+    }
+}
